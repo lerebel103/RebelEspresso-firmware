c7d9646e1342e67bf9f1a7cefb42787e942c9c12
Will Castelnau <will.castelnau@woodside.com.au>
2020-10-12 16:59:20 +0800

    Bumped priority of process loop.

--- components/rebel-espresso/src/control/process_loop.cpp
+++ components/rebel-espresso/src/control/process_loop.cpp
@@ -160,7 +160,7 @@ void process_loop_init(esp_event_loop_handle_t event_loop) {
     // Cool now create a task that will run our process loop.
     _go = true;
     ESP_ERROR_CHECK( esp_task_wdt_init(ceil(TIMER_INTERVAL0_SEC * 2), true));
-    xTaskCreate(_process_task, "process_loop", 3 * 1024, NULL, 7, &_process_task_handle);
+    xTaskCreate(_process_task, "process_loop", 3 * 1024, NULL, 10, &_process_task_handle);
     vTaskSuspend(_process_task_handle);
 
     // We also start a secondary tick loop, which for a machine wide tick that is not realtime based

86dab5022fae930520b7e9b29f86c30f63d81ac2
Will Castelnau <will.castelnau@woodside.com.au>
2020-10-23 22:02:20 +0800

    Added missing files

---
+++ ci/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Simply run cmake and run make to produce firmwares.
+#
+set -e
+
+echo "Starting build in `pwd`"
+sudo chown -R dev:dev .
+
+export HOME=/home/dev/
+sudo chown -R dev:dev ~/.ssh/
+chmod 600 ~/.ssh/id_rsa
+
+# Go, build
+mkdir build
+cd build
+
+rm ../sdkconfig || true
+cmake .. -DCMAKE_BUILD_TYPE=Release -DHARDWARE_REVISION=1.0
+make -j4
+
+
+echo "SUCCESS"

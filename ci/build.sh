d36ad73d39ca6716d770ee47176f5389d5bf8008
Will Castelnau <will.castelnau@woodside.com.au>
2020-10-23 22:08:42 +0800

    Added missing files

--- ci/build.sh
+++ ci/build.sh
@@ -7,16 +7,21 @@ set -e
 echo "Starting build in `pwd`"
 sudo chown -R dev:dev .
 
-export HOME=/home/dev/
-sudo chown -R dev:dev ~/.ssh/
-chmod 600 ~/.ssh/id_rsa
+#export HOME=/home/dev/
+#sudo chown -R dev:dev ~/.ssh/
+#chmod 600 ~/.ssh/id_rsa
 
 # Go, build
-mkdir build
-cd build
-
+mkdir build && cd build
 rm ../sdkconfig || true
 cmake .. -DCMAKE_BUILD_TYPE=Release -DHARDWARE_REVISION=1.0
+make -j4
+cd ..
+
+# Also compile unit tests
+cd unit-tests
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j4
 
 

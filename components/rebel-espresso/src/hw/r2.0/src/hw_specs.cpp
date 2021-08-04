a571a8a2af845579f5a6d2b293b9cb02e17fe33d
Will Castelnau <will.castelnau@woodside.com.au>
2021-08-04 22:07:00 +0800

    Started adding I2C bus init

--- components/rebel-espresso/src/hw/r2.0/src/hw_specs.cpp
+++ components/rebel-espresso/src/hw/r2.0/src/hw_specs.cpp
@@ -1,12 +1,32 @@
 #include <src/hw/base/boiler_temp.h>
 #include <src/hw/base/brew_temp.h>
+#include <hal/i2c_types.h>
+#include <driver/i2c.h>
+
 #include "hw_specs.h"
 #include "hw_config.h"
 
+#define I2C_MASTER_FREQ_HZ 400000
+#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
 
 void hw_specs_init(esp_event_loop_handle_t event_loop) {
     // Initialise I2C bus
+    int i2c_master_port = I2C_MASTER_NUM;
 
+    i2c_config_t conf = {
+            .mode = I2C_MODE_MASTER,
+            .sda_io_num = PIN_SDA,
+            .scl_io_num = PIN_SCL,
+            .sda_pullup_en = GPIO_PULLUP_DISABLE,
+            .scl_pullup_en = GPIO_PULLUP_DISABLE,
+            .master = {.clk_speed = I2C_MASTER_FREQ_HZ}
+    };
+
+    i2c_param_config(i2c_master_port, &conf);
+
+    ESP_ERROR_CHECK(
+            i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0));
 }
 
 void hw_specs_cfg_to_json(cJSON *root, const char *base_key) {

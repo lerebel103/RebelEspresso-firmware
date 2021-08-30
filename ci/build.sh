b0576ea5992f51f7b027825e2b8a40639df27951
Will Castelnau <will.castelnau@woodside.com.au>
2021-08-30 19:48:15 +0800

    Fixed incorrect build target dir.

--- ci/build.sh
+++ ci/build.sh
@@ -10,17 +10,22 @@ sudo chown -R dev:dev .
 export HOME=/home/dev/
 source "${IDF_PATH}"/export.sh
 
+# This is where our build artefacts will be kept
+mkdir build
+
 # Go, build
 mkdir build-r1.0 && cd build-r1.0
 rm ../sdkconfig || true
 cmake .. -DCMAKE_BUILD_TYPE=Release -DHARDWARE_REVISION=1.0 -DGIT_SUBMODULE=OFF
 make -j4
+cp -r firmware ../build
 cd ..
 
 mkdir build-r2.0 && cd build-r2.0
 rm ../sdkconfig || true
 cmake .. -DCMAKE_BUILD_TYPE=Release -DHARDWARE_REVISION=2.0 -DGIT_SUBMODULE=OFF
 make -j4
+cp -r firmware ../build
 cd ..
 
 # Also compile unit tests

b9e081c79ac2a921cacff80f8a12b4325a13f4ae
Will Castelnau <will.castelnau@woodside.com.au>
2022-01-07 06:56:31 +0800

    remove pid clear for now

--- components/rebel-espresso/src/utils/pid.cpp
+++ components/rebel-espresso/src/utils/pid.cpp
@@ -159,7 +159,8 @@ pid_result_t pid_process(
 
         } else {
             // Keep this last element just inserted only so we can get a derivative still
-            window_clear_to_tail(&pid.data_window);
+            //window_clear_to_tail(&pid.data_window);
+            //window_reset(&pid.data_window);
         }
 
         ESP_LOGI(TAG, "Calculated duty: %f, P=%f, I=%f, D=%f", duty, (cfg.P * error), (cfg.I * wdata.error_integral), (cfg.D * derivative));
